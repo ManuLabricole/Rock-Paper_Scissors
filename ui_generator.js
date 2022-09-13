7ebd5775c7dfce93ef6d70eaf465c2b02609c5d8
Manu Labricole <manu_barriol@hotmail.fr>
2022-09-13 15:53:28 +0200

    Comment avatar div creation and click function

--- ui_generator.js
+++ ui_generator.js
@@ -9,6 +9,7 @@ const avatarList = [
   "avatar_8",
 ];
 
+// Select the two containers which will be filled with avatar div
 const avatarPlayerContainer = document.querySelector(".avatar-container");
 const avatarComputerContainer = document.querySelector(
   ".computer-avatar-selection"
@@ -18,14 +19,19 @@ function getClickResult(e) {
   console.log(e.target.id);
 }
 
+// Function that create DIV and img inside. Src of img is created calling avatarList
 function addAvatar(filledDiv) {
   avatarList.forEach((name) => {
+    // Create div with classList "avatar-card"
     let newDiv = document.createElement("div");
+    // Add img with corresponding avatarList[X] avatar inside div
     let img = document.createElement("img");
     img.id = `${name}`;
     img.src = "./assets/img/avatar/" + `${name}` + ".png";
     newDiv.appendChild(img);
+
     newDiv.classList.add("avatar-card");
+    // Add the functions to each DIV per avatar
     newDiv.addEventListener("click", getClickResult);
     filledDiv.appendChild(newDiv);
   });
