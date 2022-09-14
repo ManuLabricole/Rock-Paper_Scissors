6822007fb9cc16b0470ac7e3bfcb14c1e5a001ac
Manu Labricole <manu_barriol@hotmail.fr>
2022-09-14 15:51:10 +0200

    add landingState class in html & Css

---
+++ startAnimation.js
@@ -0,0 +1,3 @@
+const playerArea = document.getElementById("player-area");
+const gameboyArea = document.getElementById("gameboy-area");
+const computerArea = document.getElementById("computer-area");
