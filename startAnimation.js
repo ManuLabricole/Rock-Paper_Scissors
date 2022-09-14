cbd37d0d94eef4ecbe4658970ea85099483b1c8b
Manu Labricole <manu_barriol@hotmail.fr>
2022-09-14 21:11:38 +0200

    set proportion of start btn and img for gameOff state

--- startAnimation.js
+++ startAnimation.js
@@ -17,21 +17,25 @@ setTimeout(loadAnimation, 10);
 // Test button interaction
 
 const startButton = document.getElementById("start-button");
-
-// function startHovered() {
-//   startButton.classList.add("detected");
-// }
-
-// startButton.addEventListener(onmouseover, (e) => {
-//   console.log(e);
-// });
-
 startButton.addEventListener("mouseenter", function (e) {
   let startButtonClass = e.target.classList.value;
   if (startButtonClass.includes("detected")) {
     console.log("already hovered");
     return;
+  } else if (startButtonClass.includes("gameOff")) {
+    return;
   } else {
     startButton.classList.add("detected");
   }
+});
+
+startButton.addEventListener("click", () => {
+  playerArea.classList.remove("landingState");
+  playerArea.classList.add("gameOff");
+  computerArea.classList.remove("landingState");
+  computerArea.classList.add("gameOff");
+  gameboyArea.classList.remove("landingState");
+  gameboyArea.classList.add("gameOff");
+  startButton.classList.remove("detected");
+  startButton.classList.add("gameOff");
 });
