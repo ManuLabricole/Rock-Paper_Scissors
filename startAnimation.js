33e5bcecc14d84f0111e0ca554df5caa5b08b352
Manu Labricole <manu_barriol@hotmail.fr>
2022-09-14 16:20:49 +0200

    add bgImg class + transition script

--- startAnimation.js
+++ startAnimation.js
@@ -1,3 +1,14 @@
+const bgImg = document.getElementById("bg_img");
 const playerArea = document.getElementById("player-area");
 const gameboyArea = document.getElementById("gameboy-area");
 const computerArea = document.getElementById("computer-area");
+
+function loadAnimation() {
+  bgImg.classList.remove("loadState");
+  bgImg.classList.add("landingState");
+  gameboyArea.classList.remove("loadState");
+  gameboyArea.classList.add("landingState");
+  console.log(gameboyArea);
+}
+
+setTimeout(loadAnimation, 1000);
