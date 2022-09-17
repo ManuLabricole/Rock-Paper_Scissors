f0eb0c5693abd85d43d5bb50e3110ea48a4d1925
Manu Labricole <manu_barriol@hotmail.fr>
2022-09-17 12:32:19 +0200

    Suppress app.js handling const - const handle in function call

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const avatarComputerContainer = document.querySelector(
 setTimeout(loadAnimation, 10); // --> ui-layout.js
 
 // Update the the layout by loading the child and store them in a const
-const avatarPlayerContainerChild = addAvatar(avatarPlayerContainer); // -->ui-layout.js
+addAvatar(avatarPlayerContainer); // -->ui-layout.js
 
 addAvatar(avatarComputerContainer); // -->ui-layout.js
 
